d3ae03ffc387547c2a5f0b00047fcb701e69ad45
박성현 <pjhomg5530@naver.com>
2023-05-19 11:53:17 +0900

    :sparkles: :: 회원가입 텍스트 클릭 시 회원가입 페이지로 이동 하기

--- presentation/src/main/java/com/yeoboya/presentation/view/SignInActivity.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/SignInActivity.kt
@@ -1,14 +1,22 @@
 package com.yeoboya.presentation.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yeoboya.presentation.R
+import com.yeoboya.presentation.databinding.ActivitySignInBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupText.setOnClickListener {
+            startActivity(Intent(this@SignInActivity, SignUpActivity::class.java))
+        }
     }
 }
