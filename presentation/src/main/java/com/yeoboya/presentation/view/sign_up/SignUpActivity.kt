45ca0980e96f57090f7d172fd7ac17f0c0f1f4ec
박성현 <pjhomg5530@naver.com>
2023-05-19 14:45:22 +0900

    :lipstick: :: 회원가입 페이지 퍼블리싱 하기

--- presentation/src/main/java/com/yeoboya/presentation/view/sign_up/SignUpActivity.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/sign_up/SignUpActivity.kt
@@ -1,12 +1,29 @@
 package com.yeoboya.presentation.view.sign_up
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yeoboya.presentation.R
+import com.yeoboya.presentation.databinding.ActivitySignUpBinding
+import com.yeoboya.presentation.view.sign_in.SignInActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val idFragment = IdFragment()
+
+        fragmentTransaction.replace(R.id.fragment_container, idFragment)
+        fragmentTransaction.commit()
+
+        binding.backArrow.setOnClickListener {
+            startActivity(Intent(this@SignUpActivity, SignInActivity::class.java))
+        }
     }
 }
