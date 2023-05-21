26b1da4da7f23caefa9c8f01f5270ef8e315b9ec
박성현 <pjhomg5530@naver.com>
2023-05-22 00:37:19 +0900

    :sparkles: :: 가입완료 버튼 클릭 시 로그인 화면으로 이동 하기

--- presentation/src/main/java/com/yeoboya/presentation/view/sign_up/PasswordFragment.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/sign_up/PasswordFragment.kt
@@ -1,20 +1,30 @@
 package com.yeoboya.presentation.view.sign_up
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yeoboya.presentation.R
+import com.yeoboya.presentation.databinding.FragmentPasswordBinding
+import com.yeoboya.presentation.view.sign_in.SignInActivity
 
 class PasswordFragment : Fragment() {
 
+    private lateinit var binding: FragmentPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_password, container, false)
+        binding = FragmentPasswordBinding.inflate(layoutInflater)
+
+        binding.signupButton.setOnClickListener {
+            startActivity(Intent(context, SignInActivity::class.java))
+        }
+
+        return binding.root
     }
 
 }
