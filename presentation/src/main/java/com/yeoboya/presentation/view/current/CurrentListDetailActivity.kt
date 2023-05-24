acb6ec592ce7e8e755a9ec48e540283711db3d22
박성현 <pjhomg5530@naver.com>
2023-05-24 23:44:02 +0900

    :sparkles: ::  CurrentListDetailActivity 만들기

---
+++ presentation/src/main/java/com/yeoboya/presentation/view/current/CurrentListDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yeoboya.presentation.view.current
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yeoboya.presentation.R
+
+class CurrentListDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_list_detail)
+    }
+}
