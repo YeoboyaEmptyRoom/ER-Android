3003a7c58fe7c6e1ad3142cc08dfade7c4755b5d
박성현 <pjhomg5530@naver.com>
2023-05-24 19:50:59 +0900

    :sparkles: :: NoticeDetailActivity 생성하기

---
+++ presentation/src/main/java/com/yeoboya/presentation/view/notice/NoticeDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yeoboya.presentation.view.notice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yeoboya.presentation.R
+
+class NoticeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice_detail)
+    }
+}
