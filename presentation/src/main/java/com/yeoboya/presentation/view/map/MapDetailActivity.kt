8f7aee893c66abad634bf3afa61effeff54714ac
박성현 <pjhomg5530@naver.com>
2023-05-24 21:04:26 +0900

    :sparkles: ::  MapDetailActivity 만들기

---
+++ presentation/src/main/java/com/yeoboya/presentation/view/map/MapDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yeoboya.presentation.view.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yeoboya.presentation.R
+
+class MapDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_detail)
+    }
+}
