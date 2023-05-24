f892994633aa0400ed36580222e9d8ec4b71ab4e
박성현 <pjhomg5530@naver.com>
2023-05-24 21:06:37 +0900

    :sparkles: ::  detailButton 클릭 시 MapDetailActivity로 이동하기

--- presentation/src/main/java/com/yeoboya/presentation/view/map/MapActivity.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/map/MapActivity.kt
@@ -1,12 +1,20 @@
 package com.yeoboya.presentation.view.map
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yeoboya.presentation.R
+import com.yeoboya.presentation.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.detailButton.setOnClickListener {
+            startActivity(Intent(this@MapActivity, MapDetailActivity::class.java))
+        }
     }
 }
