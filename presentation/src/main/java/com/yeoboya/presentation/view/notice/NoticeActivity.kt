71125d529bb0222dc3a0e16effa29e175054b204
박성현 <pjhomg5530@naver.com>
2023-05-24 17:57:34 +0900

    :sparkles: :: NoticeRecyclerView 적용하기

--- presentation/src/main/java/com/yeoboya/presentation/view/notice/NoticeActivity.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/notice/NoticeActivity.kt
@@ -2,11 +2,96 @@ package com.yeoboya.presentation.view.notice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yeoboya.presentation.R
+import com.yeoboya.domain.model.notice.NoticeModel
+import com.yeoboya.presentation.adapter.notice_recyclerview.NoticeRecyclerViewAdapter
+import com.yeoboya.presentation.databinding.ActivityNoticeBinding
 
 class NoticeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoticeBinding
+    val list = mutableListOf<NoticeModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notice)
+        binding = ActivityNoticeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        list.add(
+            NoticeModel(
+                title = "제목1",
+                writeDate = "2023.05.24",
+                description = "공지사항 입니다. 공지사항입니다. 공지사항입니다"
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목2",
+                writeDate = "2023.05.21",
+                description = "공지사항 입니다. 공지사항입니다. 공지사항입니다. 공지사항 입니다. 공지사항입니다. 공지사항입니다"
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목3",
+                writeDate = "2023.05.22",
+                description = "제목3 내용입니다."
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목4",
+                writeDate = "2023.05.24",
+                description = "공지사항을 개발하고있습니다. 공지사항을 개발하고있습니다. 공지사항을 개발하고있습니다. 공지사항을 개발하고있습니다. 공지사항을 개발하고있습니다."
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목5",
+                writeDate = "2023.05.23",
+                description = "제목5 내용입니다 \n 제목5 내용입니다. 제목5 내용입니다."
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목6",
+                writeDate = "2023.05.20",
+                description = ""
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목7",
+                writeDate = "2023.05.19",
+                description = "빈 방 서비스입니다.\n 오래 기다려주셔서 감사합니다."
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목8",
+                writeDate = "2023.04.24",
+                description = "안녕하세요 빈 방 서비스입니다."
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목9",
+                writeDate = "2023.05.21",
+                description = "내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용"
+            )
+        )
+        list.add(
+            NoticeModel(
+                title = "제목10",
+                writeDate = "2023.03.10",
+                description = "제목 10 입니다."
+            )
+        )
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val adapter = NoticeRecyclerViewAdapter(list)
+        binding.noticeRecyclerview.adapter = adapter
+        adapter.itemList = list
+        adapter.notifyDataSetChanged()
     }
 }
