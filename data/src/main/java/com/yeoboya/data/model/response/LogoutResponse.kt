172f04174eb07e11ebe537966ee75a296a4966f9
박성현 <pjhomg5530@naver.com>
2023-06-15 10:36:07 +0900

    :fire: ::  LogoutResponse 삭제

--- data/src/main/java/com/yeoboya/data/model/response/LogoutResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.yeoboya.data.model.response
-
-data class LogoutResponse(
-    val detail: String
-)
--- data/src/main/java/com/yeoboya/data/network/UserApi.kt
+++ data/src/main/java/com/yeoboya/data/network/UserApi.kt
@@ -2,7 +2,6 @@ package com.yeoboya.data.network
 
 import com.yeoboya.data.model.request.SignInRequest
 import com.yeoboya.data.model.request.SignUpRequest
-import com.yeoboya.data.model.response.LogoutResponse
 import com.yeoboya.data.model.response.UserResponse
 import retrofit2.http.POST
 
@@ -18,5 +17,5 @@ interface UserApi {
     ): UserResponse
 
     @POST("user/logout")
-    suspend fun logout(): LogoutResponse
+    suspend fun logout()
 }
