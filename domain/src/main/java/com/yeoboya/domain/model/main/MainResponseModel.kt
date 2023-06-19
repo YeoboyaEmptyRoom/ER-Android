e56d71a9d773ed55385bd325f5169efa62138e3a
박성현 <pjhomg5530@naver.com>
2023-06-19 19:51:27 +0900

    :sparkles: :: price 추가

--- domain/src/main/java/com/yeoboya/domain/model/main/MainResponseModel.kt
+++ domain/src/main/java/com/yeoboya/domain/model/main/MainResponseModel.kt
@@ -3,6 +3,7 @@ package com.yeoboya.domain.model.main
 data class MainResponseModel(
     val id: Int,
     val rent_type: String,
+    val price: String,
     val area: Float,
     val location: String,
     val room_type: String
