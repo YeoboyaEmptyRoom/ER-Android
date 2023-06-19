79bbc0fe8a75e310e637b9dae0960a3fbbc86f2f
박성현 <pjhomg5530@naver.com>
2023-06-20 01:55:15 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/yeoboya/data/model/response/main/DetailResponse.kt
+++ data/src/main/java/com/yeoboya/data/model/response/main/DetailResponse.kt
@@ -1,5 +1,7 @@
 package com.yeoboya.data.model.response.main
 
+import com.yeoboya.domain.model.main.DetailResponseModel
+
 data class DetailResponse(
     val id: Int,
     val rent_type: String,
@@ -11,4 +13,17 @@ data class DetailResponse(
     val maintenance_fee: Int,
     val parking_fee: Int,
     val description: String
+)
+
+fun DetailResponse.asDetailResponseModel() = DetailResponseModel(
+    id = id,
+    rent_type = rent_type,
+    area = area,
+    location = location,
+    floor = floor,
+    size = size,
+    room_type = room_type,
+    maintenance_fee = maintenance_fee,
+    parking_fee = parking_fee,
+    description = description
 )
