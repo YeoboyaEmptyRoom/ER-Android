ae4dc80625b91f47516d423c22d36f85a208259c
박성현 <pjhomg5530@naver.com>
2023-06-20 03:21:19 +0900

    :sparkles: :: 클릭 시 해당 상세 페이지에 필요한 데이터 가져 오기

--- presentation/src/main/java/com/yeoboya/presentation/view/current/CurrentListDetailActivity.kt
+++ presentation/src/main/java/com/yeoboya/presentation/view/current/CurrentListDetailActivity.kt
@@ -3,15 +3,20 @@ package com.yeoboya.presentation.view.current
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.bumptech.glide.Glide
 import com.yeoboya.presentation.R
 import com.yeoboya.presentation.databinding.ActivityCurrentListDetailBinding
 import com.yeoboya.presentation.view.main.MainActivity
 import com.yeoboya.presentation.view.map.MapDetailActivity
+import com.yeoboya.presentation.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CurrentListDetailActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityCurrentListDetailBinding
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCurrentListDetailBinding.inflate(layoutInflater)
@@ -28,12 +33,17 @@ class CurrentListDetailActivity : AppCompatActivity() {
     }
 
     fun initLoading() {
-        Glide.with(this).load(intent.getIntExtra("roomImage", R.drawable.home)).into(binding.horizontalCurrentImage1)
-        binding.moneyText.text = intent.getStringExtra("roomPrice")
-        binding.priceText.text = intent.getStringExtra("roomPrice")
-        binding.rulerText.text = intent.getStringExtra("roomWidthHeight")
-        binding.roomText.text = intent.getStringExtra("roomType")
-        binding.floorText.text = intent.getStringExtra("roomWidthHeight")
-        binding.locationText.text = intent.getStringExtra("roomLocation")
+        mainViewModel.getDetail(intent.getIntExtra("id", 0)) {
+            Glide.with(this).load(intent.getIntExtra("roomImage", R.drawable.home)).into(binding.horizontalCurrentImage1)
+            binding.moneyText.text = "${it.rent_type} 123"
+            binding.priceText.text = "${it.rent_type} 123"
+            binding.rulerText.text = "${it.size.toInt()}평"
+            binding.roomText.text = it.room_type
+            binding.floorText.text = "${it.floor}층"
+            binding.locationText.text = it.location
+            binding.managementText.text = "관리비 ${it.maintenance_fee}원"
+            binding.packingText.text = "주차비 ${it.parking_fee}원"
+            binding.detailText.text = it.description
+        }
     }
 }
