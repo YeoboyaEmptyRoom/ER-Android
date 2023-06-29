b1feb2212974cba23f638578497d76a138864b21
박성현 <pjhomg5530@naver.com>
2023-06-29 12:25:52 +0900

    :sparkles: :: model 생성

---
+++ data/src/main/java/com/yeoboya/data/model/response/notice/NoticeDetailResponse.kt
@@ -0,0 +1,6 @@
+package com.yeoboya.data.model.response.notice
+
+data class NoticeDetailResponse(
+    val title: String,
+    val content: String
+)
---
+++ data/src/main/java/com/yeoboya/data/model/response/notice/NoticeMessageResponse.kt
@@ -0,0 +1,5 @@
+package com.yeoboya.data.model.response.notice
+
+data class NoticeMessageResponse(
+    val message: String
+)
