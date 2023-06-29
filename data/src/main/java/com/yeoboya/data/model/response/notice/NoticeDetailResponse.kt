660e6fe144837cbc18b6282c06de54f2f6d82334
박성현 <pjhomg5530@naver.com>
2023-06-29 13:09:46 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/yeoboya/data/model/response/notice/NoticeDetailResponse.kt
+++ data/src/main/java/com/yeoboya/data/model/response/notice/NoticeDetailResponse.kt
@@ -1,6 +1,13 @@
 package com.yeoboya.data.model.response.notice
 
+import com.yeoboya.domain.model.notice.response.NoticeDetailResponseModel
+
 data class NoticeDetailResponse(
     val title: String,
     val content: String
+)
+
+fun NoticeDetailResponse.asNoticeDetailResponseModel() = NoticeDetailResponseModel(
+    title = title,
+    content = content
 )
